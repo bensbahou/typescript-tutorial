1005844b1aa70e2c7975a25167f728a3fdf2b4d0
bensbahou <younesbensbahou@gmail.com>
2023-09-07 11:41:59 +0100

    hi

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,14 @@
-var character = 'mario';
+var character = "mario";
 var age = 30;
 var isBlackBelt = false;
 // character = 20;
-character = 'luigi';
+character = "luigi";
 // age = 'yoshi';
 age = 40;
 // isBlackBelt = 'yes';
 isBlackBelt = true;
 var circ = function (diameter) {
-    return diameter * Math.PI;
+  return diameter * Math.PI;
 };
 // console.log(area('hello'));
-console.log(circ(7.5));
+console.log(circ(7.6));
